fc655156933c82c5e05d1b15f4c60d55f3805c2b
RajaniBoga <rajanib.25@gmail.com>
2025-06-27 11:31:11 +0530

    This is latest code

--- update_buyer.java
+++ update_buyer.java
@@ -7,7 +7,10 @@ import project.connectionProvider;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
+  
 
+
+  
 /**
  *
  * @author nickh
